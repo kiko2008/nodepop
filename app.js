29f9ca3bbf691012e0f1a7c7b2f06c87130c1f11
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-10-28 19:45:43 +0100

    Cambios para internacionalizacion

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const { isAPI } = require('./lib/utils');
+const loginController = require('./controllers/loginController');
 
 let app = express();
 
@@ -25,37 +28,66 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Middleware static
 app.use(express.static(path.join(__dirname, 'public')));
-// Connect to bbdd
-require('./lib/connectMongo');
-require('./models/product');
-// For api ads
-app.use('/apiv1', require('./controllers/apiv1/apiProductRouter'));
-// For web ads
-app.use('/', require('./controllers/productRouter'));
+
+// Configuramos multiidioma en express
+const i18n = require('./lib/i18nConfigure')();
+app.use(i18n.init);
 
 // app title
 app.locals.titulo='Nodepop';
 
+// Connect to bbdd
+require('./lib/connectMongo');
+require('./models/product');
+
+// For api ads
+app.post('/loginJWT', loginController.postJWT);
+app.use('/apiv1', require('./controllers/apiv1/apiProductRouter'));
+
+/**
+ * Inicializamos/cargamos la sesión del usuario que hace la petición
+ */
+app.use(session({
+    name: 'nodepop-session',
+    secret: 'hsd fkljadskjfhadssldjkh adksfhsd897sd8yf87w3yrih7wdehiuwehrfiu',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 2 * 24 * 60 * 60 * 1000, httpOnly: true }, // a los dos días de inactividad caduca
+    store: new MongoStore({
+      // como conectarse a la base de datos
+      url: process.env.MONGO_CONNECTION
+    })
+}));
+
+// auth helper middleware - dar acceso a sesión desde las vistas
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
+
 /**
  * Application web routes
  */
-//app.use('/',        require('./routes/index'));
-//app.use('/about',   require('./routes/about'));
-//app.use('/lang',    require('./routes/lang'));
-//app.use('/privado', require('./routes/privado'));
+app.use('/',        require('./controllers/index'));
+app.use('/lang',    require('./controllers/lang'));
+app.get('/private', require('./controllers/productRouter'));
 // usamos el estilo de Controladores para estructurar las rutas
-//app.get('/login', loginController.index);
-//app.post('/login', loginController.post);
-//app.get('/logout', loginController.logout);
+app.get('/login', loginController.index);
+app.post('/login', loginController.post);
+app.get('/logout', loginController.logout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
-    //next();
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+
+    // error de validación
+    if (err.name === "TokenExpiredError" || err.message==="no token provided") {
+        err.status = 401;       
+    }
 
     res.status(err.status || 500);
 
