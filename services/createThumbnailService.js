43c435c6086537cac9918eeaa7c63596f97ff1f8
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-11-04 10:50:19 +0100

    Refactorizando

--- services/createThumbnailService.js
+++ services/createThumbnailService.js
@@ -3,6 +3,7 @@
 // Service for create thumbnails with jimp
 const cote = require('cote');
 const jimp = require('jimp');
+const namedRoutes = require('../lib/namedRoutes');
 
 const responder = new cote.Responder({ name: 'create image thumbnail responder' });
 
@@ -14,7 +15,7 @@ responder.on('createThumbnail', (req, done) => {
             console.log(image);
             return image
                 .resize(100, 100) // resize
-                .write('../public/images/thumbnails/' + req.file.filename); // save
+                .write(namedRoutes.images + '/thumbnails/' + req.file.filename); // save
         }).catch(err => {
             console.error(err);
         });
